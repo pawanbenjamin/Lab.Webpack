726e6b738f9f9f8f30a0f5cf977f9daff583c503
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-06-24 09:30:43 -0500

    init

---
+++ client/app.js
@@ -0,0 +1,19 @@
+const winnerText = document.getElementById("winner");
+const leftoversBtn = document.getElementById("leftovers");
+const takeoutBtn = document.getElementById("takeout");
+
+const updateWinner = () => {
+	const currWinner = getWinner();
+	winnerText.innerText = currWinner;
+};
+
+takeoutBtn.addEventListener("click", () => {
+	voteTakeout();
+	updateWinner();
+});
+
+leftoversBtn.addEventListener("click", () => {
+	voteLeftovers();
+	updateWinner();
+});
+
